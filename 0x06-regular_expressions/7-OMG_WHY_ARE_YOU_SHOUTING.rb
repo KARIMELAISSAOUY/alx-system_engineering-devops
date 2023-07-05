f3b4d5ccadeb1070710e30f268d753b415df0cc8
ABDELKARIM <karimelaissaouy1@gmail.com>
2023-07-04 18:02:42 -0700

    0x06-regular_expressions

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression....
+# This script accepts one argument and passes it to a regular expression...
+# This Methode
 # This script should output: [RECEIVER],[FLAGS],[SENDER]
 
 puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
